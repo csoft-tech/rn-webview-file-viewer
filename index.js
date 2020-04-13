b78a29b6d11713ca0a448a6944afd60b29a508f2
Prakash Nakrani <csoft.tech.blr@gmail.com>
2020-04-13 19:54:49 +0530

    Added url validation

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
+import { View, Text } from 'react-native';
+import styles from './styles';
 
 export default function FileViewer(props) {
     
     let webViewUrl;
+    let _webView;
     const url = props.url;
     
     /**Get the extention of file */
     let ext;
     if(url) {
         ext = url.split('.').reverse()[0];
-    }
+    } 
     
     /**Check for the extention of file */
     if(ext === 'pdf') {
-        webViewUrl = `https://docs.google.com/viewerng/viewer?url=${url}`;
+        webViewUrl = `https://docs.google.com/viewerng/viewer?url=${url}`;        
     } else {
         webViewUrl = url;
     }
     
+    if(webViewUrl) {
+        _webView = <WebView source={{ uri: webViewUrl }} />;
+    } else {
+        _webView = <Text>No url found!</Text>
+    }
+
+
+    
     return (
-        <WebView source={{ uri: webViewUrl }} />
+        <View style={styles.webView}>
+            {_webView}
+        </View>
     )
 }
