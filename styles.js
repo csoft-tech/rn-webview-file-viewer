0410b0eab31a6688a893b16ffea436eec436aace
SathyaXXX03 <52455443+SathyaXXX03@users.noreply.github.com>
2021-04-21 15:28:44 +0530

    Update styles.js

--- styles.js
+++ styles.js
@@ -16,4 +16,5 @@ export default styles = StyleSheet.create({
         width: 70,
         height: 70,
     },
+    transparentView:{zIndex:1,height:60,position:'absolute',flex:1,width:100,right:0},
 })
